6f91172bc5d4a5572ee511702306210ef101b339
claudio <claudio.pereira@adyen.com>
2021-10-23 19:21:09 +0200

    Add morse decoding module

---
+++ challenge_1/morse.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import sys
+
+def morse(text):
+    char_map = {
+        'a': '.-',
+        'b': '-...',
+        'c': '-.-.',
+        'd': '-..',
+        'e': '.',
+        'f': '..-.',
+        'g': '--.',
+        'h': '....',
+        'i': '..',
+        'j': '.---',
+        'k': '-.-',
+        'l': '.-..',
+        'm': '--',
+        'n': '-.',
+        'o': '---',
+        'p': '.--.',
+        'q': '--.-',
+        'r': '.-.',
+        's': '...',
+        't': '-',
+        'u': '..--',
+        'v': '...-',
+        'w': '.--',
+        'x': '-..-',
+        'y': '-.--',
+        'z': '--..',
+        ' ': '|',
+    }
+    result = []
+    for letter in text.lower():
+        if letter not in char_map:
+            continue
+        for c in char_map[letter]:
+            result.append(c)
+        result.append(' ')
+    return ''.join(result[:-1])
+
+def morse_signal(text):
+    morse_text = morse(text)
+    signal_map = {
+        '.': '. ',
+        '-': '... ',
+        ' ': '  ',
+        '|': '      ',
+    }
+    result = []
+    for letter in morse_text:
+        result.append(signal_map[letter])
+    return ''.join(result)
+
+text = sys.argv[1]
+print(morse(text))
+print(morse_signal(text))
