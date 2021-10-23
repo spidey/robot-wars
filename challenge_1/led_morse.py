45abe169415b9f41c9bb37039bbafe1e41d426d7
Claudio Roberto França Pereira <spideybr@gmail.com>
2021-10-24 02:28:47 +0800

    New version of `led_morse.py`
    Implemented the first challenge with bonus goal, blinking "ADYEN" in
    morse code with different colors per letter.

--- challenge_1/led_morse.py
+++ challenge_1/led_morse.py
@@ -1,16 +1,9 @@
-# -*- coding:UTF-8 -*-
+#!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
 import time
 from enum import Enum
-
-#Set the GPIO port to BCM encoding mode.
-GPIO.setmode(GPIO.BCM)
-
-#RGB pins are initialized into output mode
-GPIO.setup(LED_R, GPIO.OUT)
-GPIO.setup(LED_G, GPIO.OUT)
-GPIO.setup(LED_B, GPIO.OUT)
+import morse
 
 class LEDStatus(Enum):
   OFF=0
@@ -24,47 +17,69 @@ class Color(Enum):
   TURQUOISE=4
   BLUE=5
 
+
 class LEDController:
-   def setup():
+   LED_PINS = [22, 27, 24]
+
+   def setup(self):
      #Set the GPIO port to BCM encoding mode.
      GPIO.setmode(GPIO.BCM)
-
      #RGB pins are initialized into output mode
-     GPIO.setup(LED_R, GPIO.OUT)
-     GPIO.setup(LED_G, GPIO.OUT)
-     GPIO.setup(LED_B, GPIO.OUT)
+     for pin in LEDController.LED_PINS:
+       GPIO.setup(pin, GPIO.OUT)
 
 
-   def set_rgb(led_staus):
+   def set_rgb(self, led_status):
     #Definition of RGB module pin
-    LED_PINS = {22: 0, 27: 1, 24:2}
-    for pin, index in LED_PINS:
-      GPIO.output(pin, GPIO.HIGH if led_status[index] == ON else  GPIO.LOW)
+    for index, pin in enumerate(LEDController.LED_PINS):
+      value = (GPIO.HIGH if led_status[index] == LEDStatus.ON else GPIO.LOW)
+      GPIO.output(pin, value)
 
 
 class Morser:
   color_to_led_status = {
     Color.RED: [LEDStatus.ON, LEDStatus.OFF, LEDStatus.OFF],
-    Color.NO_COLOR: [LEDStatus.ON, LEDStatus.OFF, LEDStatus.OFF]
+    Color.NO_COLOR: [LEDStatus.OFF, LEDStatus.OFF, LEDStatus.OFF],
+    Color.YELLOW: [LEDStatus.ON, LEDStatus.ON, LEDStatus.OFF],
+    Color.GREEN: [LEDStatus.OFF, LEDStatus.ON, LEDStatus.OFF],
+    Color.BLUE: [LEDStatus.OFF, LEDStatus.OFF, LEDStatus.ON],
+    Color.TURQUOISE: [LEDStatus.OFF, LEDStatus.ON, LEDStatus.ON],
   }
+
+  delay = 0.2
 
   def __init__(self, controller):
     self.controller = controller
-    self.color = Color.NO_COLOR
 
-  def set_color(color):
+  def set_color(self, color):
     self.color = color
-    self.controller.set_rgb(color_to_led_status[color])
+
+  def text(self, text):
+      signal = morse.morse_signal(text)
+      print(signal)
+      for c in signal:
+          current_color = self.color
+          if c == ' ':
+            current_color = Color.NO_COLOR
+          self.controller.set_rgb(Morser.color_to_led_status[current_color])
+          time.sleep(Morser.delay)
+
+  def letter_delay(self):
+    time.sleep(Morser.delay*3)
+
+  def word_delay(self):
+    time.sleep(Morser.delay*4)
+
 
 #Display 7 color LED
-try:
-  controller = LEDController()
-  controller.setup()
-  morser = Morser(controller)
-  while True:
-    morser.set_color(Color.RED)
-    time.sleep(1)
-    morser.set_color(Color.NO_COLOR)
-except:
-    print "except"
+controller = LEDController()
+controller.setup()
+morser = Morser(controller)
+pattern = [('a', Color.RED), ('d', Color.YELLOW), ('y', Color.GREEN), ('e', Color.TURQUOISE), ('n', Color.BLUE)]
+while True:
+  for letter, color in pattern:
+    morser.set_color(color)
+    morser.text(letter)
+    morser.letter_delay()
+  morser.word_delay()
 GPIO.cleanup()
