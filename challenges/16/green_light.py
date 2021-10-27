d6b56347275534996d7dc7ef7423b3a46a7477c4
Claudio Roberto França Pereira <spideybr@gmail.com>
2021-10-28 02:15:34 +0800

    Add challenge 16

---
+++ challenges/16/green_light.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import time
+
+#Definition of  motor pin 
+IN1 = 20
+IN2 = 21
+IN3 = 19
+IN4 = 26
+ENA = 16
+ENB = 13
+
+#Definition of  button 
+key = 8
+
+#Definition of photoresistor pin
+LdrSensorLeft = 7
+LdrSensorRight = 6
+
+#Set the GPIO port to BCM encoding mode.
+GPIO.setmode(GPIO.BCM)
+
+#Ignore warning information
+GPIO.setwarnings(False)
+
+#Motor pins are initialized into output mode
+#Key pin is initialized into input mode
+#Photoresistor pins are initialized into input mode
+def init():
+    global pwm_ENA
+    global pwm_ENB
+    GPIO.setup(ENA,GPIO.OUT,initial=GPIO.HIGH)
+    GPIO.setup(IN1,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(IN2,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(ENB,GPIO.OUT,initial=GPIO.HIGH)
+    GPIO.setup(IN3,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(IN4,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(key,GPIO.IN)
+    GPIO.setup(LdrSensorLeft,GPIO.IN)
+    GPIO.setup(LdrSensorRight,GPIO.IN)
+    #Set the PWM pin and frequency is 2000hz
+    pwm_ENA = GPIO.PWM(ENA, 2000)
+    pwm_ENB = GPIO.PWM(ENB, 2000)
+    pwm_ENA.start(0)
+    pwm_ENB.start(0)
+
+#advance
+def run():
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(100)
+    pwm_ENB.ChangeDutyCycle(100)
+
+#back
+def back():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwm_ENA.ChangeDutyCycle(50)
+    pwm_ENB.ChangeDutyCycle(50)
+
+#turn left
+def left():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(0)
+    pwm_ENB.ChangeDutyCycle(50)
+
+#turn right
+def right():
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(50)
+    pwm_ENB.ChangeDutyCycle(0)
+
+#turn left in place
+def spin_left():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(50)
+    pwm_ENB.ChangeDutyCycle(50)
+
+#turn right in place
+def spin_right():
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwm_ENA.ChangeDutyCycle(50)
+    pwm_ENB.ChangeDutyCycle(50)
+
+#brake
+def brake():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW)
+
+#Button detection
+def key_scan():
+    while GPIO.input(key):
+        pass
+    while not GPIO.input(key):
+        time.sleep(0.01)
+        if not GPIO.input(key):
+            time.sleep(0.01)
+            while not GPIO.input(key):
+                pass
+
+#delay 2s	
+time.sleep(2)
+
+#The try/except statement is used to detect errors in the try block.
+#the except statement catches the exception information and processes it.
+try:
+    init()
+    key_scan()
+    while True:
+        LdrSersorLeftValue  = GPIO.input(LdrSensorLeft);
+        LdrSersorRightValue = GPIO.input(LdrSensorRight);
+        print(f"LdrSersorLeftValue is {LdrSersorLeftValue}")
+        print(f"LdrSersorRightValue is {LdrSersorRightValue}")
+
+        if LdrSersorLeftValue == True or LdrSersorRightValue == True:
+            brake()
+        else:
+            run()
+
+except KeyboardInterrupt:
+    pass
+pwm_ENA.stop()
+pwm_ENB.stop()
+GPIO.cleanup()
+
