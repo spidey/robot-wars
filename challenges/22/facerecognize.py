433559f51edaa5947913ffc1a3e8836bfd031d61
arihSiqueira <arihane.mariano@gmail.com>
2021-10-27 19:39:05 -0300

    facerecognize

---
+++ challenges/22/facerecognize.py
@@ -0,0 +1,61 @@
+import cv2
+import datetime as dt
+from time import sleep
+import logging as log
+
+#install pip install dlib
+#install pip install cmake
+#install pip install face_recognition
+# tutorial for face recognition: https://medium.com/analytics-vidhya/installing-face-recognition-error-resolved-dlib-error-cmake-error-ca000947c8cd
+
+
+def bip():
+    print("Bip")
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log',level=log.INFO)
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+    if (len(faces) > 0):
+        bip()
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
